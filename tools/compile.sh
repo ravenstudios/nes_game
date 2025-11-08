0a899b2c8c45934e85c2b1dc05f01f8ff43fca98
Robert Dodson <robertdodson@Roberts-Mini.lan>
2025-11-07 19:25:19 -0600

    added cfg

--- tools/compile.sh
+++ tools/compile.sh
@@ -4,8 +4,8 @@ o_file=${1%.*}.o
 nes_file=${1%.*}.nes
 
 tools/cc65/bin/ca65 $1 -o $o_file -t nes  || exit 1
-tools/cc65/bin/ld65 $o_file -o $nes_file -t nes  || exit 1
-
+tools/cc65/bin/ld65 $o_file -o $nes_file -t nes --mapfile src/game.map --dbgfile src/game.dbg || exit 1
+ 
 rm $o_file
 
 /Applications/Mesen.app/Contents/MacOS/Mesen  $nes_file
