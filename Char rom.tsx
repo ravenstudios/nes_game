331b300d59ad106ce23b3eadfc9129fc4c522018
Robert Dodson <ravenstudiosrob@yahoo.com>
2025-11-13 22:02:26 -0600

    added map editor

---
+++ Char rom.tsx
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Char rom" tilewidth="8" tileheight="8" tilecount="512" columns="16">
+ <image source="char.png" width="128" height="256"/>
+ <tile id="258">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="259">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="274">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="275">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="395">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="396">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="397">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="411">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="412">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="413">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="427">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="428">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="429">
+  <properties>
+   <property name="1" type="int" value="0"/>
+  </properties>
+ </tile>
+</tileset>
